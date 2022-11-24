87703b0c564c32e4767a1eef59be8a7533ab8cf2
mdakulinin <114093070+mdakulinin@users.noreply.github.com>
2022-11-24 19:37:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from pygame.draw import *
 from random import randint
 from pygame.font import *
 
-FPS = 1
+FPS = 10
 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+WHITE = 0xFFFFFF
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
@@ -18,12 +19,16 @@ HEIGHT = 800
 FIELD_WIDTH = 1200//20
 FIELD_HEIGHT = 800//20
 
-directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+directions = ((-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0))
 
-mutation_num = [0 for x in range(16)]
+GENOME_SIZE = 16
+CELLS_NUMBER = 16
+
+mutation_num = [0 for x in range(CELLS_NUMBER)]
 
 pygame.font.init()
 f = Font(None, 16)
+f1 = Font(None, 24)
 
 class Bacteria:
     def __init__(self, x, y, genome=0):
@@ -31,14 +36,14 @@ class Bacteria:
         self.x - коррдината бактерии по x (1 - (FIELD_WIDTH - 1))
         self.y - коррдината бактерии по x (1 - (FIELD_HEIGHT-1))
         self.health - здоровье, тратится при перемещении
-        self.genome - геном бактерии, массив из 16 чисел от 0 до 15;
-        0-63 - перемещение (0 - в левый верхний угол, далее по часовой стрелке)
-        64-127 - съесть еду из клетки в соответствующей клетке (0 - левый верхний угол, далее по часовой стрелке)
+        self.genome - геном бактерии, массив из 128 чисел от 0 до 128, команды;
+        0-63 - перемещение (остаток от деления на 8, 0 - в левый верхний угол, далее по часовой стрелке)
+        64-127 - съесть еду из клетки в соответствующей клетке (остаток от деления на 8, 0 - левый верхний угол, далее по часовой стрелке)
         '''
         self.x = x
         self.y = y
-        self.health = 128
-        self.genome = [randint(0, 127) for i in range(128)]
+        self.health = 16
+        self.genome = [randint(0, GENOME_SIZE - 1) for i in range(GENOME_SIZE)]
         self.text = f.render(str(self.health), True, WHITE)
         #self.patterns = patterns
     
@@ -81,7 +86,7 @@ class Bacteria:
             #cells[self.x + directions[n][0] - 1][self.y + directions[n][1] - 1].is_poison = 0
     
     def mutate(self):
-        self.genome[randint(0, 127)] = randint(0, 127)
+        self.genome[randint(0, GENOME_SIZE - 1)] = randint(0, GENOME_SIZE - 1)
         
     def is_dead(self):
         if self.health <= 0:
@@ -113,14 +118,14 @@ class Cell:
             
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH + 250, HEIGHT))
 screen.fill(WHITE)
 bacteria = []
 cells = [[Cell(x, y) for y in range(FIELD_HEIGHT)] for x in range(FIELD_WIDTH)]
 
 clock = pygame.time.Clock()
 finished = False
-for i in range(16):
+for i in range(CELLS_NUMBER):
     bacteria.append(Bacteria(randint(1, FIELD_WIDTH - 1), randint(1, FIELD_HEIGHT - 1)))
 time = 0
 while not finished:
@@ -131,20 +136,25 @@ while not finished:
     for bac in bacteria:
         if not bac.is_dead():
             bac.draw(screen)
-        if bac.genome[time % 128] < 64:
-            bac.move(bac.genome[time % 128] % 8)
+        if bac.genome[time % GENOME_SIZE] < GENOME_SIZE // 2:
+            bac.move(bac.genome[time % GENOME_SIZE] % 8)
         else:
-            bac.eat_and_suffer((bac.genome[time % 128] - 64) % 8)
-    for i in range(20, 1200, 20):
+            bac.eat_and_suffer((bac.genome[time % GENOME_SIZE] - GENOME_SIZE // 2) % 8)
+    for i in range(20, 1220, 20):
         line(screen, BLACK, (i, 0), (i, 800))
     for i in range(20, 800, 20):
         line(screen, BLACK, (0, i), (1200, i))     
     for i in range(len(bacteria)):
         if bacteria[i].is_dead():
             bacteria[i].mutate()
-            bacteria[i].health = 50
+            bacteria[i].health = 16
             mutation_num[i] += 1
-            
+    text_time = f1.render('time = ' + str(time), True, BLACK) 
+    text_K_UP = f1.render('SPEED UP - ЛКМ', True, BLACK)
+    text_K_DOWN = f1.render('SPEED DOWN - ПКМ', True, BLACK)
+    screen.blit(text_time, (WIDTH + 10, 10))
+    screen.blit(text_K_UP, (WIDTH + 10, 40))
+    screen.blit(text_K_DOWN, (WIDTH + 10, 70))
         
     pygame.display.update()
 
@@ -153,14 +163,18 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            None
+            if event.button == 1:  #  левая кнопка мыши
+                FPS *= 2
+            if event.button == 3:  # правая кнопка мыши
+                FPS = FPS // 2        
         elif event.type == pygame.MOUSEBUTTONUP:
             None
         elif event.type == pygame.MOUSEMOTION:
             None
     time += 1
-    if time % 1000 == 0:
+    if time % 10 == 0:
         print('mutations: ', mutation_num)
-    if time % 10000 == 0:
+    if time % 100 == 0:
         print('best genome: ', bacteria[mutation_num.index(min(mutation_num))].genome)
+    
 pygame.quit()
